ffc35bda345b6f1d643ef56676e7fc0fd0f71e6c
Sarra Souissi <sarrasouissi@Sarras-MacBook-Pro-4.local>
2019-09-19 12:37:17 -0400

    config security

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-consumer_key = 'WhLlMIX9tbdjDWq4CZNGOn4M3'
-consumer_secret = 'n9jkOZl6sokzqjFu23Rq1ug0vBJo9YaYqtnpMVvvnQFtbpNpTo'
-access_token = '1167491810429689856-QqcQJVtG8CptNsY3afMuoqTgPXsd4w'
-access_token_secret = 'xSZcGEMxtQqQuQC9CusqRSRxkvsH6VNTiqiO7FocEnaye'
+consumer_key = '[your consumer key]'
+consumer_secret = '[your comsumer secret]'
+access_token = '[your access token]'
+access_token_secret = '[your access token secret]'
